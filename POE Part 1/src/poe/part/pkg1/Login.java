034446f2d19c6ebd707e3fa4307fee581f552593
DavidMellorsVC <104316401+DavidMellorsVC@users.noreply.github.com>
2022-05-10 13:11:00 +0200

    Add files via upload

--- POE Part 1/src/poe/part/pkg1/Login.java
+++ POE Part 1/src/poe/part/pkg1/Login.java
@@ -4,7 +4,8 @@
  */
 package poe.part.pkg1;
 
-import javax.swing.JOptionPane;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -12,7 +13,7 @@ import javax.swing.JOptionPane;
  */
 public class Login {
     
-    private String strUsername, strPassword, strFirstName, strLastName;
+    private String strUsername, strPassword, strFirstName, strLastName, strLoginUsername, strLoginPassword;
     
     public void setUsername(String username)
     {strUsername = username;}
@@ -38,6 +39,95 @@ public class Login {
     public String getLastName()
     {return strLastName;}
     
+    public void setLoginUsername(String loginUsername)
+    {strLoginUsername = loginUsername;}
     
+    public void setLoginPassword(String loginPassword)
+    {strLoginPassword = loginPassword;}
+    
+    public String registerUser()
+    {
+        String output;
+       
+        if(checkUserName(strUsername))
+        {
+            output = "Username succesfully captured.";
+            
+            if(checkPasswordComplexity(strPassword))
+            {
+                output = "Username and password succesfully captured.";
+            }
+            else
+            {
+                output = output + "\n\nPassword is not correctly formatted,\nplease ensure that the password contains"
+                        + " at least 8 characters, a capital letter, a number and a special character.";
+            }
+        }
+        
+        else
+        {
+            output = "Username is not correctly formatted,\nplease ensure that your username contains"
+                                                + " an underscore and is no more than 5 characters in length.";    
+        }
+         
+        return output;
+    }
+    
+    public static boolean checkUserName(String usernameInput)
+    {return usernameInput.length() <= 5 && usernameInput.contains("_");}
+    
+    public static boolean checkPasswordComplexity(String passwordInput)
+        {
+            boolean hasNum = false, hasCap = false, hasLow = false, hasSymb = false, meetsLength = false; char c;
+            
+            for (int i = 0; i < passwordInput.length(); i++)
+            {
+                c = passwordInput.charAt(i);
+                
+                if (Character.isDigit(c))
+                {hasNum = true;}
+                
+                else if (Character.isUpperCase(c))
+                {hasCap = true;}
+                
+                else if (Character.isDigit(c))
+                {hasNum = true;}
+                
+                else if (Character.isLowerCase(c))
+                {hasLow = true;}
+                
+                else if(passwordInput.length() >= 8)
+                {meetsLength = true;}
+            }
+            
+            Pattern symbolPattern = Pattern.compile("[^a-zA-Z0-9]");
+            Matcher symbolMatcher = symbolPattern.matcher(passwordInput);
+            boolean isStringContainsSpecialCharacter = symbolMatcher.find();
+         
+            if(isStringContainsSpecialCharacter)
+            {hasSymb = true;}
+            
+            if(hasNum && hasCap && hasLow && hasSymb && meetsLength)
+            {return true;}
+            
+            else
+            {return false;}
+            
+        }
+    public static boolean loginUser()
+    {
+                    
+  
+                    if(strLoginUsername.equals(strUsername) && strLoginPassword.equals(strPassword))
+                        {
+                        JOptionPane.showMessageDialog(null, "Welcome " + a.getFirstName() +", " + a.getLastName() + " it is great to see you again." ); 
+                        }
+                    else
+                        {
+                            JOptionPane.showMessageDialog(null, "Username or password incorrect, please try again.");                          
+                        }
+    }
+    
+     
     
 }
--- POE Part 1/src/poe/part/pkg1/POEPart1.java
+++ POE Part 1/src/poe/part/pkg1/POEPart1.java
@@ -21,125 +21,26 @@ public class POEPart1 {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
         String firstName = JOptionPane.showInputDialog("Please enter your first name: ");
         String lastName = JOptionPane.showInputDialog("Please enter your last name: ");
-        
+        String username = JOptionPane.showInputDialog("Please create a username: ");
+        String password = JOptionPane.showInputDialog("Please create a password: ");
         a.setFirstName(firstName);
         a.setLastName(lastName);
+        a.setUsername(username);
+        a.setPassword(password);
         
-        Username();
-        Password();
-        UserLogin();
+        JOptionPane.showMessageDialog(null, a.registerUser());
+        
+        JOptionPane.showMessageDialog(null, "Please login to your account.");
+        username = JOptionPane.showInputDialog("Please enter your username: ");
+        password = JOptionPane.showInputDialog("Please enter your password: ");
+        a.setLoginUsername(username);
+        a.setLoginPassword(password);
         
         
     }
-    
-     public static void Username()
-             {
-                 
-                
-            String username = JOptionPane.showInputDialog("Please enter a username: ");
-            
-                while(checkUserName(username) == false)
-                {    
-                    JOptionPane.showMessageDialog(null, "Username is not correctly formatted,\nplease ensure that your username contains"
-                        + " an underscore and is no more than 5 characters in length.");
-                    username = JOptionPane.showInputDialog("Please enter a username: ");
-                }
-                
-                JOptionPane.showMessageDialog(null, "Username succesfully captured");
-                a.setUsername(username);         
-            }
-     
-     public static void Password()
-     {
-         String password = JOptionPane.showInputDialog("Please enter a password: ");
-        
-            while (checkPasswordComplexity(password) == false || hasSpecialCharacters(password) == false || passwordLength(password) == false)
-            {
-                JOptionPane.showMessageDialog(null, "Password is not correctly formatted,\nplease ensure that the password contains"
-                        + " at least 8 characters, a capital letter, a number and a special character.");
-                password = JOptionPane.showInputDialog("Please enter a password: ");
-            }
-            
-            JOptionPane.showMessageDialog(null, "Password succesfully captured");
-            a.setPassword(password);
-            
-            System.out.println(a.getPassword());
-     }
-     
-     public static boolean checkUserName(String usernameInput)
-        {
-            
-            return usernameInput.length() <= 5 && usernameInput.contains("_");
-        }
-     
-     
-     
-    public static boolean passwordLength(String passwordInput)
-    {
-        if (passwordInput.length() >= 8)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    
-    
-    
-    
-    public static boolean checkPasswordComplexity(String passwordInput)
-        {
-            boolean hasNum = false; boolean hasCap = false; boolean hasLow = false; char c; 
-            
-            for (int i = 0; i < passwordInput.length(); i++)
-            {
-                c = passwordInput.charAt(i);
-                if (Character.isDigit(c))
-                {
-                    hasNum = true;
-                }
-                else if (Character.isUpperCase(c))
-                {
-                    hasCap = true;
-                }
-                else if (Character.isDigit(c))
-                {
-                    hasNum = true;
-                }
-                else if (Character.isLowerCase(c))
-                {
-                    hasLow = true;
-                }
-                if(hasNum && hasCap && hasLow)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-    
-    
-    
-    
-    
-     
-     public static boolean hasSpecialCharacters(String passwordInput)
-     {
-       
-         Pattern symbolPattern = Pattern.compile("[^a-zA-Z0-9]");
-         Matcher symbolMatcher = symbolPattern.matcher(passwordInput);
-         boolean isStringContainsSpecialCharacter = symbolMatcher.find();
-         
-         if(isStringContainsSpecialCharacter)
-         {
-            return true;
-         }
-        return false;
-     }
      
      public static void UserLogin()
         {
